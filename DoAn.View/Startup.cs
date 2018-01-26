9f3c2f561cdc76834fddf6fc1e316288c9ff136b
camnguyenitepu <hongcam941997@gmail.com>
2018-01-26 14:44:11 +0700

    14h43 26/1/2018
    create proj

---
+++ DoAn.View/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoAn.View.Startup))]
+namespace DoAn.View
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
