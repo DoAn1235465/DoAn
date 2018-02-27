914c8c87c78a2dc7827802c92927c4c277fe42ed
camnguyenitepu <hongcam941997@gmail.com>
2018-02-27 23:29:07 +0700

    update Entity.dll + Model.dll

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoAn.Model.dbSanPhamEntities db = new DoAn.Model.dbSanPhamEntities();
+            var value = db.SanPhams.Where(x => x.Id == 1);
+            Console.Write(value);
+            Console.ReadKey();
+        }
+    }
+}
