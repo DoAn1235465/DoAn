b8ac64ad9f5a2e221d4b6b7952aa4a98e3ab108d
camnguyenitepu <hongcam941997@gmail.com>
2018-03-06 14:03:28 +0700

    commit

--- DoAn.API/Controllers/SanPhamsController.cs
+++ DoAn.API/Controllers/SanPhamsController.cs
@@ -11,116 +11,126 @@ using System.Web.Http.Description;
 using DoAn.Entity;
 using DoAn.Model;
 using Newtonsoft.Json.Linq;
+using DoAn.Entity.Service;
+using System.Web.Http.Cors;
+using DoAn.API.Service;
 
 namespace DoAn.API.Controllers
 {
+	[EnableCors("*","*","*")]
     public class SanPhamsController : ApiController
     {
-        private DBSanPhamDAEntities db = new DBSanPhamDAEntities();
+		sSanPham sp = new sSanPham();
 
-        // GET: api/SanPhams
-        //public IQueryable<SanPham> GetSanPhams()
-        //{
-        //    return db.SanPhams;
-        //}
-        public JToken Get()
+        // GET: api/SanPhams/
+		[ActionName("GetLoaiSP")]
+        public JToken GetLoaiSP(int id)
         {
-            var value = db.SanPhams;
-
-            return JToken.FromObject(value); 
+            var request = sp.GetAll(id,0);
+            return JToken.FromObject(request); 
         }
+<<<<<<< HEAD
         //đâsdasdasd
+=======
+		[ActionName("GetNhomSP")]
+		public JToken GetNhomSP(int id)
+		{
+			var request = sp.GetAll(0,id);
+			return JToken.FromObject(request);
+		}
+
+>>>>>>> Duong
         // GET: api/SanPhams/5
-        [ResponseType(typeof(SanPham))]
-        public IHttpActionResult GetSanPham(int id)
-        {
-            SanPham sanPham = db.SanPhams.Find(id);
-            if (sanPham == null)
-            {
-                return NotFound();
-            }
+        //[ResponseType(typeof(SanPham))]
+        //public IHttpActionResult GetSanPham(int id)
+        //{
+        //    SanPham sanPham = db.SanPhams.Find(id);
+        //    if (sanPham == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return Ok(sanPham);
-        }
+        //    return Ok(sanPham);
+        //}
 
-        // PUT: api/SanPhams/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutSanPham(int id, SanPham sanPham)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //// PUT: api/SanPhams/5
+        //[ResponseType(typeof(void))]
+        //public IHttpActionResult PutSanPham(int id, SanPham sanPham)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            if (id != sanPham.Id)
-            {
-                return BadRequest();
-            }
+        //    if (id != sanPham.Id)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            db.Entry(sanPham).State = EntityState.Modified;
+        //    db.Entry(sanPham).State = EntityState.Modified;
 
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SanPhamExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!SanPhamExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return StatusCode(HttpStatusCode.NoContent);
-        }
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
 
-        // POST: api/SanPhams
-        [ResponseType(typeof(SanPham))]
-        public IHttpActionResult PostSanPham(SanPham sanPham)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //// POST: api/SanPhams
+        //[ResponseType(typeof(SanPham))]
+        //public IHttpActionResult PostSanPham(SanPham sanPham)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            db.SanPhams.Add(sanPham);
-            db.SaveChanges();
+        //    db.SanPhams.Add(sanPham);
+        //    db.SaveChanges();
 
-            return CreatedAtRoute("DefaultApi", new { id = sanPham.Id }, sanPham);
-        }
+        //    return CreatedAtRoute("DefaultApi", new { id = sanPham.Id }, sanPham);
+        //}
 
-        // DELETE: api/SanPhams/5
-        [ResponseType(typeof(SanPham))]
-        public IHttpActionResult DeleteSanPham(int id)
-        {
-            SanPham sanPham = db.SanPhams.Find(id);
-            if (sanPham == null)
-            {
-                return NotFound();
-            }
+        //// DELETE: api/SanPhams/5
+        //[ResponseType(typeof(SanPham))]
+        //public IHttpActionResult DeleteSanPham(int id)
+        //{
+        //    SanPham sanPham = db.SanPhams.Find(id);
+        //    if (sanPham == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            db.SanPhams.Remove(sanPham);
-            db.SaveChanges();
+        //    db.SanPhams.Remove(sanPham);
+        //    db.SaveChanges();
 
-            return Ok(sanPham);
-        }
+        //    return Ok(sanPham);
+        //}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
 
-        private bool SanPhamExists(int id)
-        {
-            return db.SanPhams.Count(e => e.Id == id) > 0;
-        }
+        //private bool SanPhamExists(int id)
+        //{
+        //    return db.SanPhams.Count(e => e.Id == id) > 0;
+        //}
     }
 }
